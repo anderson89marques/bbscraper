a974a331f3879dd06edb64df78a00c02af7c4e1b
Thiago Colares <thicolares@gmail.com>
2018-07-29 20:58:35 -0300

    Update TRANSACOES_URL
    The previous version was returning "Esta versão do aplicativo não será mais suportada."

--- bbscraper/urls.py
+++ bbscraper/urls.py
@@ -2,7 +2,7 @@ API_ENDPOINT = 'https://mobi.bb.com.br/mov-centralizador/'
 HASH_URL = 'https://mobi.bb.com.br/mov-centralizador/hash'
 LOGIN_URL = 'https://mobi.bb.com.br/mov-centralizador/servico/ServicoLogin/login'
 SALDO_URL = 'https://mobi.bb.com.br/mov-centralizador/servico/ServicoSaldo/saldo'
-TRANSACOES_URL = 'https://mobi.bb.com.br/mov-centralizador/tela/ExtratoDeContaCorrente/extrato'
+TRANSACOES_URL = 'https://mobi.bb.com.br/mov-centralizador/servico/ExtratoDeContaCorrente/extrato'
 
 # Essas url não são utilizadas pelo  script do Kamus, eu preciso entender para que servem
 POST_LOGIN_WARMUP_URL1= 'https://mobi.bb.com.br/mov-centralizador/servico/ServicoVersionamento/servicosVersionados'
